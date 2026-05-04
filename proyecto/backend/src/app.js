53e7a2090b979bbe62d418a4a6b202f76e660d47
JuanGualim <gua24852@uvg.edu.gt>
2026-05-03 23:41:35 -0600

    Agregación de ruta para el endpoint de cliente

--- proyecto/backend/src/app.js
+++ proyecto/backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import ventaRoutes from "./routes/ventaRoutes.js";
 import productoRoutes from "./routes/productoRoutes.js";
+import clienteRoutes from "./routes/clienteRoutes.js";
+
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use(express.json());
 // rutas
 app.use("/api", ventaRoutes);
 app.use("/api", productoRoutes);
+app.use("/api", clienteRoutes);
+
 
 app.get("/", (req, res) => {
     res.send("API funcionando 🚀");
