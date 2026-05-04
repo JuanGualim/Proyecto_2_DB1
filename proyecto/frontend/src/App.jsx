edc37cd034294ee539c3f48acdbd8a241f2f46a4
JuanGualim <gua24852@uvg.edu.gt>
2026-05-04 01:53:05 -0600

    Configuración de tailwind

--- proyecto/frontend/src/App.jsx
+++ proyecto/frontend/src/App.jsx
@@ -2,8 +2,11 @@ import Productos from "./pages/Productos";
 
 function App() {
   return (
-    <div>
-      <h1>Proyecto Tienda</h1>
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-blue-600 mb-4">
+        Proyecto Tienda
+      </h1>
+
       <Productos />
     </div>
   );
